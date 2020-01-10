34ebce8593df575345a69d26576ac430c178f31a
Jack <bongmusam@hotmail.com>
2020-01-10 07:45:09 +0200

    added plans service functionality and plants listing component

--- plants-finder/src/app/components/plant-listing/plant-listing.component.ts
+++ plants-finder/src/app/components/plant-listing/plant-listing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlantService } from 'src/app/services/plant.service';
+import { IPlant } from 'src/app/interfaces/plant';
 
 @Component({
   selector: 'app-plant-listing',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlantListingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private plantService: PlantService) { }
+  plants: IPlant[];
+  offset = 1;
+  limit = 15;
 
   ngOnInit() {
+    this.fetchPlants(15, this.offset);
   }
 
+  fetchPlants(limit: number, rowNumber: number) {
+    this.plantService.getPlants(this.limit, this.offset).subscribe((results: IPlant[]) => {
+      this.plants = results;
+   });
+  }
+
+  onNext() {
+   this.offset++;
+   this.fetchPlants(this.limit, this.offset);
+  }
+
+  onPrevious() {
+    if (this.offset > 0) {
+      this.offset--;
+    }
+    this.fetchPlants(this.limit, this.offset);
+  }
 }
--- plants-finder/src/app/services/plant.service.ts
+++ plants-finder/src/app/services/plant.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { IPlant } from '../interfaces/plant';
+import { timeout, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantService {
+  apiUrl: string;
+  constructor(private httpClient: HttpClient) {
+    this.apiUrl = environment.apiUrl;
+   }
 
-  constructor() { }
+   getPlants(limit: number, offset: number) {
+    return this.httpClient
+      .get<IPlant[]>(`${this.apiUrl}&$limit=${limit}&$offset=${offset}`, {
+      })
+      .pipe(timeout(10000), catchError((error: HttpErrorResponse) => {
+        return throwError(error);
+      }));
+  }
+
+  filterPlants(filterColumn: string, value: string) {
+    return this.httpClient
+      .get<IPlant[]>(`${this.apiUrl}&$limit=10&${filterColumn}=${value}`, {
+      })
+      .pipe(timeout(10000), catchError((error: HttpErrorResponse) => {
+        return throwError(error);
+      }));
+  }
 }
